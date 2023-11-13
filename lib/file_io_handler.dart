1882f47cd95acb0a442cb9b44e314230c864dc03
rakinrkz <rakinuzzaman@gmail.com>
2023-11-14 01:41:34 +0600

    no need of append, always update

--- lib/file_io_handler.dart
+++ lib/file_io_handler.dart
@@ -2,31 +2,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
-Future<void> appendToCredsFile(String id, String password) async {
-  // Get the application documents directory
-  Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
-
-  // Construct the file path for the credentials file
-  String filePath = '${appDocumentsDirectory.path}/creds.json';
-  File credsFile = File(filePath);
-
-  // Read existing content or create an empty list
-  List<dynamic> existingData = [];
-  if (credsFile.existsSync()) {
-    String fileContent = credsFile.readAsStringSync();
-    existingData = json.decode(fileContent);
-  }
-
-  dynamic userCreds = {'id': id, 'password': password};
-
-  // Append new data
-  if (!existingData.contains(userCreds)) {
-    existingData.add(userCreds);
-    // Write the updated content back to the file
-    credsFile.writeAsStringSync(json.encode(existingData));
-  }
-}
-
 Future<void> updateCredsFile(List<List<String>> updatedCreds) async {
   Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
   String filePath = '${appDocumentsDirectory.path}/creds.json';
